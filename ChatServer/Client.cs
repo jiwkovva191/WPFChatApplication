30047188a12b2d9ef82f7433bb6b35e83b4582ae
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-25 18:01:35 +0300

    added packetReader and builder to the server and client

--- ChatServer/Client.cs
+++ ChatServer/Client.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using ChatServer.Net.IO;
 
 namespace ChatServer
 {
@@ -12,11 +13,17 @@ namespace ChatServer
         public string Username { get; set; }
         public Guid UID { get; set; }
         public TcpClient ClientSocket { get; set; }
+        PacketReader _packetReader;
 
         public Client(TcpClient client)
         {
             ClientSocket = client;
             UID = Guid.NewGuid();
+            _packetReader = new PacketReader(ClientSocket.GetStream());
+
+            var opcode = _packetReader.ReadByte();
+            Username = _packetReader.ReadMessage();
+
             Console.WriteLine($"[{DateTime.Now}]: Client has connected with the username: {Username}");
         }
     }
