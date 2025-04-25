2aff1651ffe810559ec0278c7250af29715c4764
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-25 17:10:22 +0300

    added class client and configuration to program.cs

--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+namespace ChatServer
+{
+    class Program
+    {
+        static List<Client> _users;
+        static TcpListener _listener;
+        static void Main(string[] args)
+        {
+            _users = new List<Client>();
+            _listener = new TcpListener(IPAddress.Parse("127.0.0.1"), 7891);
+            _listener.Start();
+
+            while (true)
+            {
+                var client = new Client(_listener.AcceptTcpClient());
+                _users.Add(client);
+
+                /*broadcast the connection to everyone on the server*/
+            }
+
+
+        }
+    }
+}
