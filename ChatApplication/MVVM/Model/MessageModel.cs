b60e399fd2cc03d4efd79a2c334204869de20cc7
jiwkovva19 <yankowadelyana@gmail.com>
2025-04-27 15:43:14 +0300

    added style to the message

--- ChatApplication/MVVM/Model/MessageModel.cs
+++ ChatApplication/MVVM/Model/MessageModel.cs
@@ -9,11 +9,11 @@ namespace ChatApplication.MVVM.Model
     public class MessageModel
     {
         public string Username { get; set; }
-        public string UsernameColor { get; set; }
-        public string ImageSource { get; set; }
+        public string? UsernameColor { get; set; }
+        public string? ImageSource { get; set; }
         public string Message { get; set; }
-        public DateTime Time { get; set; }
-        public bool IsNativeOrigin { get; set; }
+        public DateTime? Time { get; set; }
+        public bool? IsNativeOrigin { get; set; }
         public bool? FirstMessage { get; set; }
 
     }
